a85a557abe53ab9ca05d1a86955775e9d3de2568
Ziggy <ega.nagari@gmail.com>
2016-09-06 06:16:16 +0700

    Update lockercallscriptshare.js

--- lockercallscriptshare.js
+++ lockercallscriptshare.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
   var sandi = $(location).attr('href'); 
   $('#default-usage .to-lock').sociallocker({
-    buttons: {order:["google-plus"]},
+    buttons: {order:["facebook-share"]},
     facebook: {like:{url:"https://www.facebook.com/PGRIWebsite/"}, share:{appId:859273720857036, url:"http://bloganalyzer.blogspot.com"},},
     text: {header: "Cara DOWNLOAD", message: "Klik Share untuk menampilkan LINK Download - sebagai Donasi"}, 
     locker: {close: false, timer: 0,},
